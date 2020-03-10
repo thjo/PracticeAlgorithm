66524efe162264ad61d2c3f0ec8aa2a73b16abce
Takhoon Jo <fortunejth@gmail.com>
2020-03-09 22:16:06 -0400

    Add project files.

---
+++ PracticeAlgorithm/Arrays.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAlgorithm
+{
+    [TestClass]
+    public class Arrays
+    {
+        void Print(object output)
+        {
+            System.Diagnostics.Debug.WriteLine(output);
+        }
+
+        [TestMethod]
+        public void DS2DArray()
+        {
+            int[,] arr = new int[,] {
+                 {0, -4, -6, 0, -7, -6}
+                ,{-1, -2, -6, -8, -3, -1}
+                ,{-8, -4, -2, -8, -8, -6}
+                ,{-3, -1, -2, -5, -7, -4}
+                ,{-3, -5, -3, -6, -6, -6}
+                ,{ -3, -6, 0, -8, -6, -7}
+            };
+
+            int result = hourglassSum(arr);
+            Print(result);
+        }
+        private int hourglassSum(int[,] arr)
+        {
+            int? maxhourglassSum = null;
+
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    int max = arr[row,col] + arr[row,col + 1] + arr[row,col + 2] +
+                            arr[row + 1,col + 1] +
+                            arr[row + 2,col] + arr[row + 2,col + 1] + arr[row + 2,col + 2];
+                    Print(max);
+                    if(maxhourglassSum == null || maxhourglassSum.Value < max)
+                        maxhourglassSum = max;
+                }
+            }
+
+            return maxhourglassSum.Value;
+        }
+    }
+}
