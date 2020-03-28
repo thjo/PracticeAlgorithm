005fde8099cdab60980cb7213172b67aa6768ed4
Takhoon Jo <fortunejth@gmail.com>
2020-03-28 11:32:22 -0400

    Kangaroo

--- PracticeAlgorithm/Implementation.cs
+++ PracticeAlgorithm/Implementation.cs
@@ -34,19 +34,19 @@ namespace PracticeAlgorithm
             int cntApples = 0, cntOranges = 0;
             for(int i = 0; i < maxLen; i++)
             {
-                if (i < apples.Length && isMidNum(s, t, apples[i]))
+                if (i < apples.Length && isMidNum(s, t, a, apples[i]))
                     cntApples++;
-                if (i < oranges.Length && isMidNum(s , t, oranges[i]))
+                if (i < oranges.Length && isMidNum(s , t, b, oranges[i]))
                     cntOranges++;
             }
 
             Print(cntApples);
             Print(cntOranges);
         }
-        private bool isMidNum(int s, int t, int n)
+        private bool isMidNum(int s, int t, int p, int n)
         {
             bool isMid = false;
-
+            n += p;
             if (n <= t && n >= s)
                 isMid = true;
 
@@ -54,5 +54,31 @@ namespace PracticeAlgorithm
         }
         #endregion
 
+
+        #region | Kangaroo | 
+
+        [TestMethod]
+        public void KangarooA()
+        {
+            Print(kangaroo(0, 3, 4, 2));
+            Print(kangaroo(21, 6, 47, 3));
+        }
+        private string kangaroo(int x1, int v1, int x2, int v2)
+        {
+            double n = -1;
+
+            if( v1 - v2 > 0)
+            {
+                n = (x2 - x1) / ((v1 - v2)*1.0);
+                if( n * 10 % 10  != 0)
+                {
+                    //Not integer
+                    n = -1;
+                }
+            }
+
+            return n > 0 ? "YES" : "NO";
+        }
+        #endregion
     }
 }
