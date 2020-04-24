ef16bb10b7c69e5528afa140a24352469ba00417
Takhoon Jo <fortunejth@gmail.com>
2020-04-24 14:08:35 -0400

    1

--- PracticeAlgorithm/leetcode_practice1.cs
+++ PracticeAlgorithm/leetcode_practice1.cs
@@ -1143,6 +1143,59 @@ namespace PracticeAlgorithm
 
         #endregion
 
+        #region | Regular Expression Matching | 
+
+        [TestMethod]
+        public void IsMatch()
+        {
+            Print(IsMatch("aaa", "ab*a*c*a"));
+            Print(IsMatch("ab", ".*"));
+            Print(IsMatch("aab", "c*a*b"));
+            Print(IsMatch("mississippi", "mis*is*p*"));
+        }
+        /// <summary>
+        /// https://www.youtube.com/watch?v=l3hda49XcDE&list=PLrmLmBdmIlpuE5GEMDXWf0PWbBD9Ga1lO
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public bool IsMatch(string s, string p)
+        {
+            bool[][] dp = new bool[s.Length + 1][];
+            for (int i = 0; i <= s.Length; i++)
+            {
+                dp[i] = new bool[p.Length + 1];
+                dp[i][0] = false;
+            }
+
+            dp[0][0] = true;
+            for (int i = 1; i <= p.Length; i++)
+            {
+                if (p[i - 1] == '*')
+                    dp[0][i] = dp[0][i - 2];
+            }
+
+
+            for (int i = 1; i <=s.Length; i++)
+            {
+                for (int j = 1; j <= p.Length; j++)
+                {
+                    if (p[j - 1] == '.' || p[j - 1] == s[i - 1])
+                        dp[i][j] = dp[i - 1][j - 1];
+                    else if (p[j - 1] == '*')
+                    {
+                        dp[i][j] = dp[i][j - 2];
+                        if (dp[i][j] == false && (p[j - 2] == '.' || p[j - 2] == s[i - 1]))
+                            dp[i][j] = dp[i-1][j];
+                    }
+                    else
+                        dp[i][j] = false;
+                }
+            }
+
+            return dp[s.Length][p.Length];
+        }
+        #endregion
 
 
 
