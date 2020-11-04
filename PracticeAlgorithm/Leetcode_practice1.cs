081051732084d385721e6db881cc1f4cd8e6a043
Takhoon Jo <fortunejth@gmail.com>
2020-11-04 00:01:19 -0500

    77

--- PracticeAlgorithm/Leetcode_practice1.cs
+++ PracticeAlgorithm/Leetcode_practice1.cs
@@ -2875,7 +2875,19 @@ namespace PracticeAlgorithm
 
         public ListNode OddEvenList(ListNode head)
         {
-            return null;
+            if (head == null) return null;
+
+            ListNode odd = head, even = head.next, evenHead = even;
+            while (even != null && even.next != null)
+            {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+
+            return head;
         }
         #endregion
 
